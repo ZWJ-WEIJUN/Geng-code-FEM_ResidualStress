7d54306a3709936f9275de3d18ab96d90acf1e2b
Makar Loktyukhin <zhangweijun_zwj@icloud.com>
2023-12-29 16:34:35 -0800

    Nothing changed, just comments out for diff plots

--- SimulationData_Extraction.py
+++ SimulationData_Extraction.py
@@ -15,66 +15,66 @@ from matplotlib.ticker import FormatStrFormatter
 #     dp_dedl = np.load(f)
 
 
-# with open('thinwall_test_lp_control_lp1.7_04032023.npy', 'rb') as lp:
-#     nodal_T2 = np.load(lp)
-#     nodal_A2 = np.load(lp)
-#     LSC = np.load(lp)
-#     energy_lose = np.load(lp)
-#     laser_energy = np.load(lp)
-#     median_temperature = np.load(lp)
-#     lp_layer = np.load(lp)
-#     # fr_layer = np.load(lp)
-#     dp_error = np.load(lp)
-#     dp_dedl = np.load(lp)
+with open('thinwall_test_lp_control_lp1.7_04032023.npy', 'rb') as lp:
+    nodal_T2 = np.load(lp)
+    nodal_A2 = np.load(lp)
+    LSC = np.load(lp)
+    energy_lose = np.load(lp)
+    laser_energy = np.load(lp)
+    median_temperature = np.load(lp)
+    lp_layer = np.load(lp)
+    # fr_layer = np.load(lp)
+    dp_error = np.load(lp)
+    dp_dedl = np.load(lp)
 
 
-with open('thinwall_test_no_control_lp1.7_09162023.npy', 'rb') as f:
-    nodal_T2 = np.load(f)   # nodal_T2 * nodal_volume * heat_capacity
-    nodal_A2 = np.load(f)  # activate
-    LSC = np.load(f)
-    energy_lose = np.load(f)
-    laser_energy = np.load(f)
-    median_temperature = np.load(f)
-    lp_layer = np.load(f)
-    fr_layer = np.load(f)
-    dp_error = np.load(f)
-    dp_dedl = np.load(f)
+# with open('thinwall_test_no_control_lp1.7_09162023.npy', 'rb') as f:
+#     nodal_T2 = np.load(f)   # nodal_T2 * nodal_volume * heat_capacity
+#     nodal_A2 = np.load(f)  # activate
+#     LSC = np.load(f)
+#     energy_lose = np.load(f)
+#     laser_energy = np.load(f)
+#     median_temperature = np.load(f)
+#     lp_layer = np.load(f)
+#     fr_layer = np.load(f)
+#     dp_error = np.load(f)
+#     dp_dedl = np.load(f)
 
 # print(energy_lose)
 # print(fr_layer)
 # print(nodal_T2.size)
 
-# # *** Plot Laser Power ***
-# plt.rcParams["font.weight"] = "bold"
-# layer = np.arange(1, 51, dtype=np.int)
-# print(lp_layer)
-# fig, ax1 = plt.subplots()
-# color = 'indianred'
-# lns1 = ax1.plot(layer, lp_layer, linewidth=3, marker='.', markersize=10,
-#                 color=color, label='Laser power')
-# ax1.tick_params(axis='y', labelcolor=color)
-# ax1.set_xlabel("Layer #")
-# ax1.set_ylabel("Laser Power (kW)", color=color, fontweight='bold')
+# *** Plot Laser Power ***
+plt.rcParams["font.weight"] = "bold"
+layer = np.arange(1, 51, dtype=np.int)
+print(lp_layer)
+fig, ax1 = plt.subplots()
+color = 'indianred'
+lns1 = ax1.plot(layer, lp_layer, linewidth=3, marker='.', markersize=10,
+                color=color, label='Laser power')
+ax1.tick_params(axis='y', labelcolor=color)
+ax1.set_xlabel("Layer #")
+ax1.set_ylabel("Laser Power (kW)", color=color, fontweight='bold')
 
-# ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-# color = 'tab:blue'
-# ax2.set_ylabel('Substrate Temperature ($^\circ$C)',
-#                color=color, fontweight='bold')
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+color = 'tab:blue'
+ax2.set_ylabel('Substrate Temperature ($^\circ$C)',
+               color=color, fontweight='bold')
 
-# lns2 = ax2.plot(layer, np.full((50, ), 600),
-#                 linestyle='dashed', linewidth=3, color='blue', label='Objective Temperature')
-# # ax2.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
-# lns3 = ax2.plot(layer, median_temperature, color=color,
-#                 linewidth=3, marker=".", markersize=10, label='Medium Temperature')
-# ax2.tick_params(axis='y', labelcolor=color)
+lns2 = ax2.plot(layer, np.full((50, ), 600),
+                linestyle='dashed', linewidth=3, color='blue', label='Objective Temperature')
+# ax2.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+lns3 = ax2.plot(layer, median_temperature, color=color,
+                linewidth=3, marker=".", markersize=10, label='Medium Temperature')
+ax2.tick_params(axis='y', labelcolor=color)
 
-# lns = lns1+lns2+lns3
-# labs = [l.get_label() for l in lns]
+lns = lns1+lns2+lns3
+labs = [l.get_label() for l in lns]
 
-# plt.legend(lns, labs, loc='lower center', framealpha=0.2)
-# plt.title("Laser Power-Based Energy Control Simulation", fontname="Arial Black",
-#           size=15, fontweight="bold")
-# # ***********************
+plt.legend(lns, labs, loc='lower center', framealpha=0.2)
+plt.title("Laser Power-Based Energy Control Simulation", fontname="Arial Black",
+          size=15, fontweight="bold")
+# ***********************
 
 # # #*** Plot Feedrate ***
 # plt.rcParams["font.weight"] = "bold"
@@ -109,40 +109,40 @@ with open('thinwall_test_no_control_lp1.7_09162023.npy', 'rb') as f:
 
 # plt.title("Feed Rate-Based Energy Control Simulation", fontname="Arial Black",
 #           size=15, fontweight="bold")
-# ***********************
+# #***********************
 
-# #*** Plot No Ctrl ***
-plt.rcParams["font.weight"] = "bold"
-layer = np.arange(1, 51, dtype=np.int)
-print(fr_layer*60)
-fig, ax1 = plt.subplots()
-color = 'tab:purple'
-lns1 = ax1.plot(layer, fr_layer*60, linewidth=2, marker='.', markersize=5,
-                color=color, label='Feed rate (mm/min)')
-# ax1.tick_params(axis='y', labelcolor=color)
-ax1.set_xlabel("Layer #", fontweight='bold')
-ax1.set_ylabel("Deposition Parameters", fontweight='bold')
-color = 'indianred'
-lns2 = ax1.plot(layer, lp_layer*1000, linewidth=2, marker='.', markersize=5,
-                color=color, label='Laser power (W)')
+# # #*** Plot No Ctrl ***
+# plt.rcParams["font.weight"] = "bold"
+# layer = np.arange(1, 51, dtype=np.int)
+# print(fr_layer*60)
+# fig, ax1 = plt.subplots()
+# color = 'tab:purple'
+# lns1 = ax1.plot(layer, fr_layer*60, linewidth=2, marker='.', markersize=5,
+#                 color=color, label='Feed rate (mm/min)')
+# # ax1.tick_params(axis='y', labelcolor=color)
+# ax1.set_xlabel("Layer #", fontweight='bold')
+# ax1.set_ylabel("Deposition Parameters", fontweight='bold')
+# color = 'indianred'
+# lns2 = ax1.plot(layer, lp_layer*1000, linewidth=2, marker='.', markersize=5,
+#                 color=color, label='Laser power (W)')
 
-ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-color = 'tab:blue'
-ax2.set_ylabel('Substrate Temperature ($^\circ$C)',
-               color=color, fontweight='bold')
-lns3 = ax2.plot(layer, np.full((50, ), 600),
-                linestyle='dashed', linewidth=3, color='blue', label='Objective Temperature')
-lns4 = ax2.plot(layer, median_temperature, linewidth=3, marker='.',
-                color=color, label='Medium Temperature')
-ax2.tick_params(axis='y', labelcolor=color)
-lns = lns1+lns2+lns3+lns4
-labs = [l.get_label() for l in lns]
-plt.legend(lns, labs, loc='center right', framealpha=0.2)
+# ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+# color = 'tab:blue'
+# ax2.set_ylabel('Substrate Temperature ($^\circ$C)',
+#                color=color, fontweight='bold')
+# lns3 = ax2.plot(layer, np.full((50, ), 600),
+#                 linestyle='dashed', linewidth=3, color='blue', label='Objective Temperature')
+# lns4 = ax2.plot(layer, median_temperature, linewidth=3, marker='.',
+#                 color=color, label='Medium Temperature')
+# ax2.tick_params(axis='y', labelcolor=color)
+# lns = lns1+lns2+lns3+lns4
+# labs = [l.get_label() for l in lns]
+# plt.legend(lns, labs, loc='center right', framealpha=0.2)
 
-plt.title("No Control Simulation", fontname="Arial Black",
-          size=15, fontweight="bold")
+# plt.title("No Control Simulation", fontname="Arial Black",
+#           size=15, fontweight="bold")
 
 
-# # ***********************
+# # # ***********************
 
 plt.show()
