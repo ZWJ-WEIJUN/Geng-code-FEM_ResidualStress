c421a8945bf2c3b3d6a9840a57fab6f65863c07f
WZ <65190311+ZWJ-WEIJUN@users.noreply.github.com>
2024-03-11 05:42:50 -0700

    Add video processing functionality, speeding up and cutting first 15s, to FrameExtraction_aviVideo.py

--- FrameExtraction_aviVideo.py
+++ FrameExtraction_aviVideo.py
@@ -1,5 +1,24 @@
 import cv2
 
+
+
+from moviepy.editor import VideoFileClip
+import cv2
+from moviepy.editor import VideoFileClip, vfx
+
+
+# ************************************************************Code for speeding up and cutting a video file************************************************************
+clip = VideoFileClip("/Users/zhangweijun/Documents/GitHub/Geng-code-FEM_ResidualStress/STW70_Fr_Ctrl_run20240204.avi")
+clip_speedup = clip.fx(vfx.speedx, factor=5)  # Adjust the speed factor as needed
+# clip_speedup.write_videofile("/Users/zhangweijun/Documents/GitHub/Geng-code-FEM_ResidualStress/STW70_Fr_Ctrl_run20240204_SpeedUp.mp4")
+
+# Cut the first 15 seconds of the video
+clip_cut = clip_speedup.subclip(15)
+clip_cut.write_videofile("/Users/zhangweijun/Documents/GitHub/Geng-code-FEM_ResidualStress/STW70_Fr_Ctrl_run20240204_SpeedUp_Cut15s.mp4")
+# ************************************************************Code for speeding up and cutting a video file************************************************************
+
+
+# ************************************************************Code for extracting frames from a video file************************************************************
 def extract_frames(video_path, output_folder):
     # Open the video file
     video = cv2.VideoCapture(video_path)
@@ -30,7 +49,9 @@ def extract_frames(video_path, output_folder):
 
     print(f"Frames extracted: {frame_count}")
 
-# Example usage
-video_path = "/Users/zhangweijun/Documents/GitHub/Geng-code-FEM_ResidualStress/STW70_LP_CTRL_run20240204.avi"
-output_folder = "/Users/zhangweijun/Documents/GitHub/Geng-code-FEM_ResidualStress/STW70_LP_CTRL_run20240204_Frames"
-extract_frames(video_path, output_folder)
+# # Example usage
+# video_path = "/Users/zhangweijun/Documents/GitHub/Geng-code-FEM_ResidualStress/STW70_LP_CTRL_run20240204.avi"
+# output_folder = "/Users/zhangweijun/Documents/GitHub/Geng-code-FEM_ResidualStress/STW70_LP_CTRL_run20240204_Frames"
+# extract_frames(video_path, output_folder)
+    
+# ************************************************************Code for extracting frames from a video file************************************************************
